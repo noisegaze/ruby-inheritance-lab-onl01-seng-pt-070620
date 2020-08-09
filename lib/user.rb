a2e15f04aee0e3f847e44173fc414d13e2092e2b
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-09 16:46:48 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,31 @@
+require 'pry' 
+
+class User 
+  
+  attr_accessor :first_name, :last_name 
+  
+  def initialize 
+    @knowledge = []
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
